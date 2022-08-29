ae491a3a6aacbaa8428f1137b38fcd1f697b6228
meysam <mysm.shahbazi@gmail.com>
2022-08-29 10:21:38 +0430

    add initi kernel

--- um/main.cu
+++ um/main.cu
@@ -11,6 +11,19 @@ void add(int n,float *x,float *y)
         y[i] = x[i] + y[i];
 }
 
+__global__
+void init(int n, float *x,float *y)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x*gridDim.x;
+
+    for (int i =index; i <n; i+=stride)
+    {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+}
+
 
 int main(void)
 {
@@ -20,15 +33,19 @@ int main(void)
     cudaMallocManaged(&x, N*sizeof(float));
     cudaMallocManaged(&y, N*sizeof(float));
 
-    for(int i=0; i<N; i++)
-    {
-        x[i] = 1.0f;
-        y[i] = 2.0f;
-    }
+    // for(int i=0; i<N; i++)
+    // {
+    //     x[i] = 1.0f;
+    //     y[i] = 2.0f;
+    // }
 
     int block_size = 256;
     int num_blocks = (N + block_size-1)/block_size;
+
+    init<<<num_blocks,block_size>>>(N, x, y);
+    
     add<<<num_blocks,block_size>>>(N,x,y);
+
     cudaDeviceSynchronize();
 
     float maxError = 0.0f;
